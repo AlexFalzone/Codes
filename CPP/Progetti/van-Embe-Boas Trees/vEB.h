341acc08d6e03ef9786bd9ecac2490dd83bf6aec
Alex <falzonealex@protonmail.com>
2020-12-17 12:02:22 +0100

    Update vEB.h

---
+++ CPP/Progetti/van-Embe-Boas Trees/vEB.h
@@ -0,0 +1,24 @@
+#include<cmath>
+class van_Emde_Boas
+{
+private:
+    int universe;
+    int min;
+    int max;
+
+    van_Emde_Boas* cluster;
+    van_Emde_Boas* summary;
+
+    /*
+    *   
+    */
+    int high(int x)
+    {
+        int lower_square = pow(2, floor(log2(u)/2);
+        return floor(x / lower_square);
+    }
+
+public:
+
+
+};
