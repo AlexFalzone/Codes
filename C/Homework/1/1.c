f2878d54e0608ffa6bd1d5ea232065d9da774f70
AlexFalz <falzonealex@protonmail.com>
2021-05-27 16:55:53 +0200

    M

--- C/Homework/1/1.c
+++ C/Homework/1/1.c
@@ -67,7 +67,7 @@ int main(int argc, char *argv[])
             perror(buffer);
             exit(1);
         }
-        
+        w
         do
         {
             if ((size = read(sd, buffer, BUFSIZ)) == -1)
