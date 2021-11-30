c46f2c6e4ca6c760245df53880eccfe7e8d74842
Alex Falzone <falzonealex@protonmail.com>
2021-11-30 15:33:46 +0100

    M

--- C/Sistemi Distribuiti/Esercizi Socket/es.2/server_D.c
+++ C/Sistemi Distribuiti/Esercizi Socket/es.2/server_D.c
@@ -1,8 +1,10 @@
 /*
-    Estendere le funzionalità dal server definito al punto precedente realizzando un secondo
-    server chiamato ​Server B ​che oltre a stampare il messaggio ricevuto sullo standard
-    output, lo invia come risposta al client (senza modificarlo). Testare il server usando
-    telnet.
+    Estendere le funzionalità dal server definito al punto precedente realizzando un quarto
+    server chiamato ​Server D ​modificando il comportamento del metodo “​int
+    MUL(String str)​” che dovrà restituire il prodotto delle singole cifre numeriche
+    presenti nella stringa in input, ad esempio per la stringa “1234” restituirà l’intero 24. Il
+    risultato del metodo deve quindi essere inviato come messaggio di risposta al client.
+    Testare il server usando telnet.
 */
 
 #include <errno.h>
@@ -19,11 +21,24 @@
 #define FPORT 7777
 #define MAXLEN 1024
 
+int MUL(char str[MAXLEN])
+{
+    int somma = 0;
+
+    for (int i = 0; i < strlen(str); i++)
+    {
+        somma += str[i] - '0';
+    }
+    return somma;
+}
+
+
 int main(int argc, char  *argv[])
 {
     int server_socket, check_bind, check_listen, connected_socket, retcode;
     struct sockaddr_in server_addr, client_addr;
-    int buffer[MAXLEN];
+    char buffer[MAXLEN];
+    int risultato = 0;
     
     if ((server_socket = socket(AF_INET, SOCK_STREAM, 0)) == 0)
     {
@@ -84,6 +99,18 @@ int main(int argc, char  *argv[])
                 printf("errno: %d\n", errno);
                 exit(EXIT_FAILURE);
             }
+
+            risultato = MUL(buffer);
+            memset(buffer, risultato, sizeof(buffer));
+            printf("%d", risultato);
+
+            if ((write(connected_socket, buffer, sizeof(risultato))) == -1)
+            {
+                perror("Errore write\n");
+                printf("errno: %d\n", errno);
+                exit(EXIT_FAILURE);
+            }
+
         }
     }
     close(server_socket);
